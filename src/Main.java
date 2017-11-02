b0dc7228f7469871f05c9214ea4bed12e34a8f72
Dana <Dana@Dana-PC.computer.Carter-PC.com>
2017-11-01 19:49:45 -0700

    Calculated horizontal and vertical displacement

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,37 @@
-import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String args[]) {
 		JFrame mainmenu = new JFrame("The Baseball Trajectory Calculator - by Carter Weiss");
 		JPanel panel = new JPanel();
 		JLabel exitvelocityindicator = new JLabel("Exit Velocity:");
-		JTextField exitvelocity = new JTextField(3);
+		JTextField exitvelocityfield = new JTextField(3);
 		JLabel launchangleindicator = new JLabel("Launch Angle:");
-		JTextField launchangle = new JTextField(3);
+		JTextField launchanglefield = new JTextField(3);
+		JButton calculate = new JButton("Begin Calculations");
 		panel.add(exitvelocityindicator);
-		panel.add(exitvelocity);
+		panel.add(exitvelocityfield);
 		panel.add(launchangleindicator);
-		panel.add(launchangle);
+		panel.add(launchanglefield);
+		panel.add(calculate);
+		calculate.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				int exitvelocity = Integer.parseInt(exitvelocityfield.getText());
+				int launchangle = Integer.parseInt(launchanglefield.getText());
+				int x = (int) (exitvelocity * Math.cos(launchangle));
+				int y = (int) (exitvelocity * Math.sin(launchangle));
+			}
+		});
 		mainmenu.add(panel);
 		mainmenu.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		mainmenu.pack();
