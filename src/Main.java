496c4a9f582d1056955e036071080899e389bf29
Dana <Dana@Dana-PC.computer.Carter-PC.com>
2017-11-01 19:36:18 -0700

    Fixed text box issue

--- src/Main.java
+++ src/Main.java
@@ -6,9 +6,9 @@ public class Main {
 		JFrame mainmenu = new JFrame("The Baseball Trajectory Calculator - by Carter Weiss");
 		JPanel panel = new JPanel();
 		JLabel exitvelocityindicator = new JLabel("Exit Velocity:");
-		JTextField exitvelocity = new JTextField();
+		JTextField exitvelocity = new JTextField(3);
 		JLabel launchangleindicator = new JLabel("Launch Angle:");
-		JTextField launchangle = new JTextField();
+		JTextField launchangle = new JTextField(3);
 		panel.add(exitvelocityindicator);
 		panel.add(exitvelocity);
 		panel.add(launchangleindicator);
